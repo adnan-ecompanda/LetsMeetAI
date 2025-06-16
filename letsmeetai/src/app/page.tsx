dd9dd55afbf5639e31c8217094e13bf0882bf23d
adnan-ecompanda <daniweber3561@gmail.com>
2025-06-17 01:03:28 +0500

    Adnan Database setup

--- letsmeetai/src/app/page.tsx
+++ letsmeetai/src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold text-green-500">Hello World</div>
+    <Button>Button</Button>
+
   );
 }
